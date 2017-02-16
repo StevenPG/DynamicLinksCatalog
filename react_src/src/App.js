50d96ee933ce3dc3dadec8987f574d2eee43ae50
Steven Gantz <steven.p.gantz@gmail.com>
2017-02-16 17:50:04 -0500

    Started building React UI frontend with fake data

--- react_src/src/App.js
+++ react_src/src/App.js
@@ -1,18 +1,89 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+// Top bar
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Dialog from 'material-ui/Dialog';
+import IconButton from 'material-ui/IconButton';
+import NavigationRefresh from 'material-ui/svg-icons/navigation/refresh';
+
+// Individual cards
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+
 import './App.css';
 
+// Needed for onTouchTap
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 class App extends Component {
+
+  state = {
+    open: false,
+  };
+
+  // Refreshing the page
+  handleRefresh = () => {
+    this.forceUpdate();
+  }
+  // End refresh method
+
+  // Settings functions
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+  // End Settings functions
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <div className="App">
+          <MuiThemeProvider>
+            <AppBar
+            title="Title"
+            iconElementLeft={<IconButton><NavigationRefresh /></IconButton>}
+            iconElementRight={<FlatButton label="Settings" onTouchTap={this.handleOpen}/>}
+            />
+          </MuiThemeProvider>
+          <MuiThemeProvider>
+            <Dialog
+              title="Application Settings"
+              actions={ <FlatButton label="Cancel" primary={true} onTouchTap={this.handleClose} />}
+              modal={false}
+              open={this.state.open}
+              onRequestClose={this.handleClose}>
+              This Modal will soon contain the application settings, along with simple authentication.
+            </Dialog>
+          </MuiThemeProvider>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Card">
+          <MuiThemeProvider>
+            <Card>
+                <CardHeader
+                  title="Title"
+                  subtitle="Subtitle"
+                  avatar="http://fresnostate.edu/webresources/images/128x128/128x128-youtube.png"
+                  actAsExpander={true}
+                  showExpandableButton={true}
+                />
+                <CardActions>
+                  <FlatButton label="Action1" />
+                  <FlatButton label="Action2" />
+                </CardActions>
+                <CardText expandable={true}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                  Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                  Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+                </CardText>
+              </Card>
+          </MuiThemeProvider>
+        </div>
       </div>
     );
   }
