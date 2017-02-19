5f3a19273cba61833b5db50533c170b4bef4b46f
Steven Gantz <steven.p.gantz@gmail.com>
2017-02-19 13:31:26 -0500

    Client side only change, configuration now fully through JSON input

--- react_src/src/App.js
+++ react_src/src/App.js
@@ -150,6 +150,7 @@ class App extends Component {
               contentStyle={{position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}}
               repositionOnUpdate={ false }
               onRequestClose={this.handleClose}>
+              	
                 <TextField
                 id="jsonconfig"
                 multiLine={true}
